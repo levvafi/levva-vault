58295b4b742137556198353f3a5d9f17a3822560
rudewalt <rudewalt@gmail.com>
2024-10-07 14:15:01 +0300

    Feature/deploy improve (#12)
    * add indexed in events, add event
    
    * update deploy script

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,5 +1,5 @@
 import { task } from 'hardhat/config';
-import { parseEther } from 'ethers';
+import { parseEther, formatEther, Signer } from 'ethers';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { loadDeployConfig } from '../deploy/config';
 import { makeDeploy } from '../deploy/index';
@@ -7,20 +7,22 @@ import { ethers } from '@nomiclabs/hardhat-ethers'; //do not remove this import
 
 interface DeployArgs {
   networkName: string;
-  creatorPrivateKey: string;
+  creatorKey: string;
+  isPrivateKey: boolean;
 }
 
 /*
  *  Deploy on fork / dry-run deploy:
- *  npx hardhat --network hardhat --config hardhat-arb-fork.config.ts task:deploy --network-name <network-name> --creator-private-key <private-key>
+ *  npx hardhat --network hardhat --config hardhat-arb-fork.config.ts task:deploy --network-name <network-name> --creator-key <private-key>
  *
  *  Deploy on real network:
- *  npx hardhat --network <network-name> --config hardhat.config.ts task:deploy --network-name <network-name> --creator-private-key <private-key>
+ *  npx hardhat --network <network-name> --config hardhat.config.ts task:deploy --network-name <network-name> --creator-key <private-key> --is-private-key
  */
 
 task('task:deploy', 'Deploy vaults')
   .addParam<string>('networkName', 'Network name')
-  .addParam<string>('creatorPrivateKey', 'Private key of contracts creator')
+  .addParam<string>('creatorKey', 'Private or public key of contracts creator')
+  .addFlag('isPrivateKey', 'If passed `creatorKey` is the private one')
   .setAction(async (taskArgs: DeployArgs, hre: HardhatRuntimeEnvironment) => {
     const dryRun =
       hre.config.networks.hardhat.forking !== undefined ? hre.config.networks.hardhat.forking.enabled : false;
@@ -29,30 +31,38 @@ task('task:deploy', 'Deploy vaults')
     if (!dryRun && hre.network.name.toLowerCase() !== network) {
       throw new Error(`The network from the config and from CLI "--network" must be same!`);
     }
-    const signer = new hre.ethers.Wallet(taskArgs.creatorPrivateKey, hre.ethers.provider);
 
-    if (dryRun) {
-      console.log(`Dry run command on fork`);
-      const blockNumber = await hre.ethers.provider.getBlockNumber();
-      console.log(`Fork block number: ${blockNumber}`);
+    const provider = hre.ethers.provider;
+
+    let signer: Signer;
+    if (taskArgs.isPrivateKey) {
+      signer = new hre.ethers.Wallet(taskArgs.creatorKey, provider);
+      console.log(`Signer from private key: ${await signer.getAddress()}`);
+    } else if (dryRun) {
+      console.log(`Impersonating signer: ${taskArgs.creatorKey}`);
+      signer = await hre.ethers.getImpersonatedSigner(taskArgs.creatorKey);
+      console.log('Funding signer');
       const [user] = await hre.ethers.getSigners();
+      const signerAddress = await signer.getAddress();
       await user.sendTransaction({
-        to: signer.address,
+        to: signerAddress,
         value: parseEther('1.0'),
       });
-      console.log(`Sent 1 ETH to deployer${signer.address}`);
+      console.log(`Sent 1 ETH to deployer${signerAddress}`);
+    } else {
+      throw new Error("Can't impersonate signer while not dry-running");
     }
 
     const config = await loadDeployConfig(network, signer.provider!, dryRun);
 
-    const balanceBefore = await signer.provider!.getBalance(signer.address);
-    console.log(`Balance before: ${hre.ethers.formatEther(balanceBefore)} Eth`);
+    const balanceBefore = await signer.provider!.getBalance(signer);
+    console.log(`Balance before: ${formatEther(balanceBefore)} Eth`);
 
     await makeDeploy(signer, config, network, dryRun, hre);
 
-    const balanceAfter = await signer.provider!.getBalance(signer.address);
-    console.log(`Balance after: ${hre.ethers.formatEther(balanceAfter)} Eth`);
+    const balanceAfter = await signer.provider!.getBalance(signer);
+    console.log(`Balance after: ${formatEther(balanceAfter)} Eth`);
 
-    console.log(`Spent: ${hre.ethers.formatEther(balanceBefore - balanceAfter)} Eth`);
+    console.log(`Spent for deploy: ${formatEther(balanceBefore - balanceAfter)} Eth`);
     console.log(`Done!`);
   });
